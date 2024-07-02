483ffc288ee3db57fcfacc1d478ba17bd24efda4
jitsunarii <jitsunari931027@gmail.com>
2024-07-02 11:09:55 +0900

    テスト

---
+++ SF2/index.js
@@ -0,0 +1,24 @@
+// import { StreetFighterGame } from "./StreetFighterGame.js";
+
+// const GameViewport = {
+//     WIDTH: 384,
+//     HEIGHT: 224,
+// };
+
+// function populateMoveDropdown() {
+//     const dropdown = document.getElementById('state-dropdown');
+
+//     Object.entries(FighterState).forEach(([, value]) => {
+//         const option = document.createElement('option');
+//         option.setAttribute('value', value);
+//         option.innerText = value;
+//         dropdown.appendChild(option);
+//     });
+// }
+
+// window.addEventListener("load", function () {
+//   new StreetFighterGame().start();
+// });
+window.onload = function(){
+    console.log('document is ready!');
+}
