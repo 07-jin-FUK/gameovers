28bd0d4b095f8c87fbe456449ac08cfd02d5ce1e
jitsunarii <jitsunari931027@gmail.com>
2024-07-02 11:29:28 +0900

    canvas全部黄色

--- SF2/index.js
+++ SF2/index.js
@@ -19,10 +19,19 @@
 // window.addEventListener("load", function () {
 //   new StreetFighterGame().start();
 // });
+
+const GameViewport = {
+  WIDTH: 384,
+  HEIGHT: 224,
+  SCALE: 4,
+};
 window.onload = function () {
   const canvasEl = document.querySelector("canvas");
   const context = canvasEl.getContext("2d");
   canvasEl.width = 384;
   canvasEl.height = 224;
+
+  canvasEl.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
+  canvasEl.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}`;
   console.log(context);
 };
