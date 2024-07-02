f4a5686a6edeb4b5c3e24ef701d5e8f3993da581
jitsunarii <jitsunari931027@gmail.com>
2024-07-02 11:25:11 +0900

    canvas調整

--- SF2/index.js
+++ SF2/index.js
@@ -19,6 +19,10 @@
 // window.addEventListener("load", function () {
 //   new StreetFighterGame().start();
 // });
-window.onload = function(){
-    console.log('document is ready!');
-}
+window.onload = function () {
+  const canvasEl = document.querySelector("canvas");
+  const context = canvasEl.getContext("2d");
+  canvasEl.width = 384;
+  canvasEl.height = 224;
+  console.log(context);
+};
