b99b0b9c669979e21d6f4c27b6e2f5986928f2fa
jitsunarii <jitsunari931027@gmail.com>
2024-07-02 11:42:33 +0900

    yellow strokeテストまで

--- SF2/index.js
+++ SF2/index.js
@@ -23,15 +23,16 @@
 const GameViewport = {
   WIDTH: 384,
   HEIGHT: 224,
-  SCALE: 4,
 };
 window.onload = function () {
   const canvasEl = document.querySelector("canvas");
   const context = canvasEl.getContext("2d");
-  canvasEl.width = 384;
-  canvasEl.height = 224;
+  canvasEl.width = GameViewport.WIDTH;
+  canvasEl.height = GameViewport.HEIGHT;
 
-  canvasEl.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
-  canvasEl.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
+  context.strokeStyle = "yellow";
+  context.moveTo(0, 0);
+  context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+  context.stroke();
   console.log(context);
 };
